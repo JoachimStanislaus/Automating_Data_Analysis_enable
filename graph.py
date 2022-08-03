e2f00d37738348a2e003429967ee365cf24aa618
JoachimStanislaus <Joachimtan00@gmail.com>
2022-08-03 15:23:42 +0800

    added date functionality, multiple select of centres & more

--- graph.py
+++ graph.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import datetime
 import matplotlib
 import matplotlib.pyplot as plt
 import sys
-from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QFileDialog, QCheckBox, QComboBox
+from PyQt5 import uic,QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QFileDialog, QCheckBox, QComboBox, QListWidget
 
 def read_file(FileDir): # Takes in file directory & Reads it
     #print(type(FileDir[0]))
@@ -12,9 +13,8 @@ def read_file(FileDir): # Takes in file directory & Reads it
 
 def main(centre,checklist):
     CreatedDataframe = (read_file(fname))
-    print('This is main')
-    print(centre)
-    print(checklist)
+    #print(centre)
+    #print(checklist)
     totalchecklist = ''
     countchecklist = 0
     for x in checklist:
@@ -28,8 +28,17 @@ def main(centre,checklist):
     SortedbyName['Shortfall'] = SortedbyName['Capacity'] - (SortedbyName['Enrollment'] + SortedbyName['Waitlist'])
     SortedbyName = SortedbyName.loc[centre]
     SortedbyName.sort_values(by='RepDate',inplace=True)
-    SortedbyName.set_index("RepDate", inplace = True)
-    SortedbyName[checklist].plot(marker='.',markersize=10, title=f'{totalchecklist} {centre}', figsize=(10,6)).get_figure().savefig(f'{totalchecklist}{centre}.png')
+    #print(SortedbyName)
+    if len(centre)>1:
+        SortedbyName=(SortedbyName.groupby(SortedbyName['RepDate'].dt.date).sum())
+        start_date = datetime.date(int(start_year),1,1)
+        end_date = datetime.date(int(end_year),12,31)
+        SortedbyName = (SortedbyName.loc[start_date:end_date])
+    else:
+        SortedbyName.set_index("RepDate", inplace = True)
+        #print(SortedbyName)
+    
+    SortedbyName[checklist].plot(marker='.',markersize=10, title=f'{totalchecklist} {centre}', figsize=(10,6)).get_figure().savefig(f'{totalchecklist}.png')
     #plt.show()
 
 
@@ -45,22 +54,33 @@ class UI(QMainWindow):
         # Define our widgets
         self.port_from_button = self.findChild(QPushButton,"Browse")
         self.port_from_label = self.findChild(QLabel,"FileName")
-        self.scroller = self.findChild(QComboBox,"Scroller")
+        self.Scroll_List = self.findChild(QListWidget,"Scroll_List")
         self.Capacity_CB = self.findChild(QCheckBox,"Capacity_CheckBox")
         self.Enrollment_CB = self.findChild(QCheckBox,"Enrollment_CheckBox")
         self.Waitlist_CB = self.findChild(QCheckBox,"Waitlist_CheckBox")
         self.Vacancy_CB = self.findChild(QCheckBox,"Vacancy_CheckBox")
         self.Shortfall_CB = self.findChild(QCheckBox,"Shortfall_CheckBox")
+        self.Start_Year_Scroller = self.findChild(QComboBox,"Start_Year")
+        self.End_Year_Scroller = self.findChild(QComboBox,"End_Year")
         self.submit_button = self.findChild(QPushButton,"Submit")
         self.quit_button = self.findChild(QPushButton,"Quit")
 
         # Click the dropdown box
+        self.Scroll_List.itemClicked.connect(self.printItemText)
         self.port_from_button.clicked.connect(self.port_from_clicker)
         self.submit_button.clicked.connect(self.submit_click)
         self.quit_button.clicked.connect(self.quit_click)
 
         # Show the app
         self.show()
+
+    def printItemText(self):
+        items = self.Scroll_List.selectedItems()
+        global Scroll_Selected_Items
+        Scroll_Selected_Items = []
+        for i in range(len(items)):
+            Scroll_Selected_Items.append(str(self.Scroll_List.selectedItems()[i].text()))    
+        print(Scroll_Selected_Items)
 
     def checked_box(self): #checks which boxes are ticked and adds them to a list
         list_of_checkboxes = [self.Capacity_CB, self.Enrollment_CB, self.Waitlist_CB, self.Vacancy_CB, self.Shortfall_CB]
@@ -74,15 +94,25 @@ class UI(QMainWindow):
         self.close() # closes the window
     
     def submit_click(self): # to run when submit button is clicked
-        #print(self.scroller.currentText())
+        global start_year, end_year
+        start_year = self.Start_Year_Scroller.currentText()
+        end_year = self.End_Year_Scroller.currentText()
         #print(self.checked_box())
         self.close() # closes the window
-        main(self.scroller.currentText(),self.checked_box())
+        main(Scroll_Selected_Items,self.checked_box())
     
     def load_data(self): # Loads data and puts the different centre names into the scroller
-        #print()
-        self.scroller.addItems((read_file(fname))['SPName'].unique()) # search for centre names and puts them in a list into the scroller
-        self.scroller.adjustSize() # adjust scroller size to fit biggest string
+        self.Scroll_List.addItems((read_file(fname))['SPName'].unique()) # search for centre names and puts them in a list into the scroller
+        self.Scroll_List.setMinimumWidth(self.Scroll_List.sizeHintForColumn(0)) # adjust scroller size to fit biggest string
+        emptylst=[]
+        for x in ((read_file(fname))['RepDate'].unique()):
+            x = (x[-4:])
+            emptylst.append(x)
+        sortedlst = (list(set(emptylst)))
+        self.Start_Year_Scroller.addItems(sortedlst) # search for centre names and puts them in a list into the scroller
+        self.Start_Year_Scroller.adjustSize() # adjust scroller size to fit biggest string
+        self.End_Year_Scroller.addItems(sortedlst) # search for centre names and puts them in a list into the scroller
+        self.End_Year_Scroller.adjustSize() # adjust scroller size to fit biggest string
 
     def port_from_clicker(self):
         global PortFromFile, Port_From_Path, fname
